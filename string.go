ecd8f1ccda5dd29904083e94cf38633cef7b551b
MhdAmir <m.amir.hs19@gmail.com>
2023-06-29 22:47:51 +0700

    be more complicated

---
+++ string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Muhammad")
+	fmt.Println("Amir")
+	fmt.Println("Abdurrozaq")
+
+	fmt.Println(len("Muhammad"))
+	fmt.Println("Amir"[0])
+}
---
+++ var.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "Muhammad Amir Abdurrozaq"
+	fmt.Println(name)
+
+	name = "Ya ndak tau"
+	fmt.Println(name)
+
+	var named = "aku"
+	fmt.Println(named)
+
+	var umur int8 = 30
+	fmt.Println(umur)
+
+	coma := 3.14
+	fmt.Println(coma)
+
+	var (
+		first_name = "Muhammad"
+		last_name  = "Amir"
+	)
+	fmt.Println("first name : ", first_name)
+	fmt.Println("last name :", last_name)
+}
